a9ff4bcd23c15011e29d4f32ea2e9bac72e280a1
MoniRanj <ranj.monica@gmail.com>
2025-03-11 15:04:16 +0530

    my third commit

--- src/test/java/Pack1/practice1.java
+++ src/test/java/Pack1/practice1.java
@@ -1,5 +1,14 @@
 package Pack1;
 
-public class practice1 {
+import org.testng.annotations.Test;
 
+public class practice1 {
+	@Test
+	public void mt1()
+	{
+	System.out.println("my new organization");	
+	}
+	
+	
+	
 }
